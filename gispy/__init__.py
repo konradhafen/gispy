481671e404acd571d4d3dacf5aa7f82790dd5b6e
KONRADSPC\khafe <konrad.hafen@gmail.com>
2018-02-13 14:22:26 -0800

    fleshing out package structure

---
+++ gispy/__init__.py
@@ -0,0 +1,5 @@
+import raster
+import vector
+
+def gispy_test():
+    return("this is the gispy package")
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='gispy',
+      version='0.0.1',
+      description='GIS functions with GDAL/OGR',
+      url="https://github.com/konradhafen/gispy",
+      author='Konrad Hafen',
+      author_email='khafen74@gmail.com',
+      license='GPLv3',
+      zip_safe=False)
