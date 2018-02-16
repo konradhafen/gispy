9aed82988e47f083e0ca6683220c9cf7ee111299
KONRADSPC\khafe <konrad.hafen@gmail.com>
2018-02-16 11:08:31 -0800

    adding more vector functions

--- gispy/vector.py
+++ gispy/vector.py
@@ -1,9 +1,41 @@
-from osgeo import ogr, osr
+from osgeo import gdal, ogr, osr
 import os
 import geopandas as gpd
 
 def vector_test():
     return "this is the vector module of the gispy package"
+
+def copyFields(lyr, lyrdefn):
+    """
+    Create fields in a new layer based on fields from an existing OGRLayerDefn.
+
+    Args:
+        lyr: OGRLayer to create fields for.
+        lyrdefn: OGRLayerDefn to copy from.
+
+    Returns:
+        OGRLayer with fields from lyrdefn added.
+
+    """
+    for i in range(0, lyrdefn.GetFieldCount()):
+        fldefn = lyrdefn.GetFieldDefn(i)
+        lyr.CreateField(fldefn)
+    return lyr
+
+def copyFieldValues(newfeat, copyfeat, defn):
+    """
+    Copy field values from one feature to another.
+
+    Args:
+        newfeat: OGRFeature to copy to
+        copyfeat: OGRFeature containing field values to copy
+        defn: OGRFieldDefinition of copyfeat
+
+    Returns:
+
+    """
+    for i in range(0, defn.GetFieldCount()):
+        newfeat.SetField(defn.GetFieldDefn(i).GetNameRef(), copyfeat.GetField(i))
 
 def createFields(lyr, fieldnames, fieldtype = ogr.OFTReal):
     """
@@ -25,23 +57,40 @@ def createFields(lyr, fieldnames, fieldtype = ogr.OFTReal):
         lyr.CreateField(field)
     return lyr
 
-def copyFields(lyr, lyrdefn):
-    for i in range(0, lyrdefn.GetFieldCount()):
-        fldefn = lyrdefn.GetFieldDefn(i)
-        lyr.CreateField(fldefn)
-    return lyr
+def createOGRDataSource(filename, driver='ESRI Shapefile'):
+    """
+    Create OGRDataSource
+    Args:
+        filename: Path of file to create
+        driver: Name of OGR Driver to use (default: 'ESRI Shapefile')
 
-def copyFieldValues(newfeat, copyfeat, defn):
-    for i in range(0, defn.GetFieldCount()):
-        newfeat.SetField(defn.GetFieldDefn(i).GetNameRef(), copyfeat.GetField(i))
+    Returns:
+        OGRDataSource
 
-def createOGRDataSource(filename, driver='ESRI Shapefile', layertype=ogr.wkbPoint, srs=None ):
+    """
     driver = ogr.GetDriverByName(driver)
     if os.path.exists(filename):
         driver.DeleteDataSource(filename)
     ds = driver.CreateDataSource(filename)
-    lyr = ds.CreateLayer(os.path.split(filename)[0], srs=srs, geom_type=layertype)
-    return ds, lyr
+    return ds
+
+def deleteFields(filename, fields):
+    """
+    Delete fields from shapefile
+
+    Args:
+        filename: Path to shapefile.
+        fields: List of fields to delete
+
+    Returns:
+
+    """
+    ds = gdal.OpenEx(filename, gdal.OF_VECTOR | gdal.OF_UPDATE)
+    layername = getFilenameWithoutExtenstion(filename)
+    for field in fields:
+        sql = 'ALTER TABLE ' + layername + ' DROP COLUMN ' + field
+        ds.ExecuteSQL(sql)
+    ds = None
 
 def getFieldValues(dataset, fieldName):
     """
@@ -66,6 +115,20 @@ def getFieldValues(dataset, fieldName):
         return values, fids
     else:
         return None
+
+def getFilenameWithoutExtenstion(filepath):
+    """
+    Return the name of a file without the director or extension
+
+    Args:
+        filepath: Path to file.
+
+    Returns:
+        string: name of file without extension
+
+    """
+    base = os.path.basename(filepath)
+    return os.path.splitext(base)[0]
 
 def joinZonalStatsToSHP(inshp, zsresult, id, stats, fieldnames, stattype=ogr.OFTReal):
     """
@@ -113,6 +176,17 @@ def fieldExists(lyr, fieldname):
         return False
 
 def openOGRDataSource(file, access=0):
+    """
+    Open and existing OGRDataSource.
+
+    Args:
+        file: Path of file to open.
+        access: Access type, 0 read only, 1 update (default: 0)
+
+    Returns:
+        OGRDataSource
+
+    """
     ds = ogr.Open(file, access)
     if ds is not None:
         return ds
@@ -120,12 +194,24 @@ def openOGRDataSource(file, access=0):
         print 'Problem opening shapefile'
 
 def reprojectShapefileLayer(shapefile, newshapefile, newsrs):
+    """
+    Create a new shapefile with a different projection.
+
+    Args:
+        shapefile: Path of shapefile to reproject.
+        newshapefile: Path of shapefile to create with new projection.
+        newsrs: OSRSpatialReference of the new shapefile.
+
+    Returns:
+
+    """
     ds = openOGRDataSource(shapefile)
     layer = ds.GetLayer()
     srs = layer.GetSpatialRef()
     transform = osr.CoordinateTransformation(srs, newsrs)
 
-    outds, outlyr = createOGRDataSource(newshapefile, srs=newsrs, layertype=layer.GetGeomType())
+    outds = createOGRDataSource(newshapefile)
+    outlyr = lyr = ds.CreateLayer(getFilenameWithoutExtenstion(newshapefile), srs=newsrs, geom_type=layer.GetGeomType())
     copyFields(outlyr, layer.GetLayerDefn())
     defn = outlyr.GetLayerDefn()
     feat = layer.GetNextFeature()
@@ -140,3 +226,42 @@ def reprojectShapefileLayer(shapefile, newshapefile, newsrs):
         feat = layer.GetNextFeature()
     ds = None
     outds = None
+
+def saveFilteredLayerAsShapefile(filename, layer, filter, driver='ESRI Shapefile', close=False):
+    """
+    Filter OGRLayer with SQL statement and save as a shapefile
+    Args:
+        filename: Path to save new shapefile.
+        layer: Layer to apply SQL filter to.
+        filter: Filter to apply to layer.
+        driver: Name of OGR driver used to create the new shapefile (default: ESRI Shapefile)
+        close: If the new OGRDataSource and OGRLayer should be closed. False: data source and layer returned, True: data source and layer closed (default: False)
+
+    Returns:
+        OGRDataSource and OGRLayer if 'close' is False.
+
+    """
+    layer.SetAttributeFilter(filter)
+    return saveLayerAsShapefile(filename, layer, driver, close)
+
+def saveLayerAsShapefile(filename, layer, driver='ESRI Shapefile', close=False):
+    """
+    Save a vector layer to a new OGR DataSource.
+
+    Args:
+        filename: Name of new data source/shapefile
+        layer: Layer to copy.
+        driver: Name of driver to create DataSource (default: ESRI Shapefile)
+        close: Close created DataSource and layer, if True new ds and new layer will be deleted, if False they will be returned (default: False)
+
+    Returns:
+        OGRDataSource and OGRLayer, if 'close' is False.
+
+    """
+    ds = createOGRDataSource(filename, driver)
+    newlyr = ds.CopyLayer(layer, getFilenameWithoutExtenstion(filename))
+    if close:
+        del ds, newlyr
+        return None
+    else:
+        return ds, newlyr
