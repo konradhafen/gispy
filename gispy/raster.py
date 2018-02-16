da5fd553d85448510e05d67231cede372293d6cb
KONRADSPC\khafe <konrad.hafen@gmail.com>
2018-02-16 13:04:20 -0800

    adding raster-vector operations in separate file

--- gispy/raster.py
+++ gispy/raster.py
@@ -1,69 +1,14 @@
 from osgeo import gdal, ogr, osr
 import numpy as np
-import vector
 from scipy import stats
 import os
 
 def raster_test():
     return "this is the raster module of the gispy package"
 
-def clipByFeature(inputdir, outputdir, rasterfiles, shapefile, fieldname, nodata=-9999, xres=None, yres=None):
-    """
-    Clip multiple rasters by features in a shapefile. Creates a new directory for each feature and saves clipped rasters in the directory
 
-    Note:
-        All input rasters and shapefiles should have the same spatial reference
 
-    Args:
-        inputdir: directory where rasters to be clipped are located
-        outputdir: directory where a new directory for each clipped raster will be created
-        rasterfiles: list of raster file names and extensions to be clipped. e.g. ['raster1.tif', 'raster2.tif]
-        shapefile: shapefile containing features to clip with
-        fieldname: name of field to select feature with
-        nodata: nodata value (default: -9999)
-        xres: x resolution of output raster (default: None), with default resolution is taken from the input rater
-        yres: y resolution of output rater (defaul: None), with default resolution is taken from the input rater
 
-    Returns:
-        None
-
-    """
-    fieldValues, fids = vector.getFieldValues(shapefile, fieldname)
-    fieldValues_unique = list(set(fieldValues)) #get unique field values (otherwise the same operation may be done twice)
-    for value in fieldValues_unique:
-        dirvalue = outputdir + "/" + str(value)
-        if not os.path.isdir(dirvalue):
-            os.mkdir(dirvalue)
-        for raster in rasterfiles:
-            infile = inputdir + "/" + str(raster)
-            if os.path.exists(infile):
-                clipRasterWithPolygon(infile, shapefile, dirvalue + "/" + str(raster), nodata=nodata, xres=xres, yres=yres,
-                                      fieldValue=value, field=fieldname)
-
-def clipRasterWithPolygon(rasterpath, polygonpath, outputpath, nodata=-9999, xres=None, yres=None, field=None, fieldValue=None):
-    """
-
-    Args:
-        rasterpath: raster to clip
-        polygonpath: shapefile containing features to clip with
-        outputpath: path of output clipped raster
-        nodata: nodata value (default: -9999)
-        xres: x resolution of output raster (default: None, resolution of input raster)
-        yres: y resolution of output raster (default: None, resolution of input raster)
-        field: name of shapefile field to select features and name directories
-        fieldValue: list of unique values for input field
-
-    Returns:
-
-    """
-    if xres is None or yres is None: xres, yres = getXYResolution(rasterpath)
-    if field is not None and fieldValue is not None: wexp = str(field) + " = \'" + str(fieldValue) + "\'"
-    else: wexp = None
-
-    warpOptions = gdal.WarpOptions(format='GTiff', cutlineDSName=polygonpath, cropToCutline=True, cutlineWhere=wexp, xRes=xres, yRes=abs(yres), dstNodata=nodata)
-    gdal.WarpOptions()
-    gdal.Warp(outputpath, rasterpath, options=warpOptions)
-    return None
 
 def createBandIndex(rasterPath, minValue, maxValue):
     """
@@ -335,48 +280,7 @@ def percentileOfMultibandIndex(datapath, index, percentilepath, scorepath=None, 
 
     return None
 
-def polygonToRaster(rasterpath, vectorpath, fieldname, rows, cols, geot, prj, allcells=False, nodata=-9999, datatype = gdal.GDT_Float32):
-    """
-    Convert polygon shapefile to raster dataset.
-
-    Args:
-        rasterpath: Path of raster to be created.
-        vectorpath: Path of polygon shapefile to rasterize.
-        fieldname: Name of shapefile field to use as values in new raster.
-        rows: Number of rows in new raster.
-        cols: Number of columns in new raster.
-        geot: Affine geotransform of new raster.
-        prj: Spatial reference of new raster.
-        allcells: If all cells intersected by polygons should be rasterized, or just when polygon includes cell center (defaul: False)
-        datatype: GDAL datatype of new raster (default: gdal.GDT_Float32)
-
-    Returns:
-
-    """
-    inds = ogr.Open(vectorpath)
-    lyr = inds.GetLayer()
-    outds = gdal.GetDriverByName('GTiff').Create(rasterpath, cols, rows, 1, datatype)
-    outds.SetProjection(prj)
-    outds.SetGeoTransform(geot)
-    band = outds.GetRasterBand(1).ReadAsArray()
-    band.fill(nodata)
-    outds.GetRasterBand(1).WriteArray(band)
-    outds.GetRasterBand(1).SetNoDataValue(nodata)
-
-    ALL_TOUCHED = 'FALSE'
-    if allcells: ALL_TOUCHED = 'TRUE'
-
-    if vector.fieldExists(lyr, fieldname):
-        status = gdal.RasterizeLayer(outds, [1], lyr, options=['ALL_TOUCHED='+ALL_TOUCHED, 'ATTRIBUTE='+fieldname, 'NODATA='+str(nodata)])
-        if status is not 0:
-            print "Rasterize not successful"
-    else:
-        print "Rasterize field does not exist"
-
-    outds = None
-    return None
-
-def writeArrayAsGTiff(path, array, rows, cols, geot, srs, nodata=-9999, nan=-9999, type=gdal.GDT_Float32):
+def writeArrayAsGTiff(path, array, rows, cols, geot, srs, nodata=-9999, nan=-9999, datatype=gdal.GDT_Float32):
     """
     Write array to a GeoTiff raster
 
@@ -389,13 +293,13 @@ def writeArrayAsGTiff(path, array, rows, cols, geot, srs, nodata=-9999, nan=-999
         srs: spatial reference for the output raster
         nodata: no data value for the output raster
         nan: value in array that should be written as nodata
-        type: gdal data type of output raster (default: GDT_Float32)
+        datatype: gdal data type of output raster (default: GDT_Float32)
 
     Returns:
         None
 
     """
-    ds = gdal.GetDriverByName("GTiff").Create(path, xsize=cols, ysize=rows, bands=1, eType=type)
+    ds = gdal.GetDriverByName("GTiff").Create(path, xsize=cols, ysize=rows, bands=1, eType=datatype)
     ds.SetProjection(srs)
     ds.SetGeoTransform(geot)
     array = np.where((array==np.nan) | (array==nan), nodata, array)
